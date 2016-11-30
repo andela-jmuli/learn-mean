b0270cc23f7c63b031dd8dc92c1907d335cdade4
andela-jmuli <joseph.muli@andela.com>
2016-11-30 16:12:01 +0300

    Add variable hello

--- typescript/variables.ts
+++ typescript/variables.ts
@@ -4,6 +4,8 @@ var burger = 'hamburger',
     calories = 300,
     tasty = true;
 
+var hello = "say hello";
+
 function speak(food: string, energy: number){
     console.log("Our "+ food + " has "+ energy+ " calories.");
 }
